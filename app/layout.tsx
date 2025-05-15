dc13c868fa9157b382489a9de631cda04f778308
officialjp <joaopedroofficial05@gmail.com>
2025-05-15 15:03:28 +0100

    Added statistics and added logo to hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <SpeedInsights />
         <Footer />
       </body>
     </html>
