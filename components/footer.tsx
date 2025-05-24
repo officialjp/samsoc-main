095bc5798ccf40ac8d59aa6a391c056db1859461
natski <59707870+natspooky@users.noreply.github.com>
2025-05-24 22:21:05 +0100

    yippe

--- components/footer.tsx
+++ components/footer.tsx
@@ -46,8 +46,8 @@ export function Footer() {
 			</div>
 			<div className="border-t border-gray-200 py-6 text-center">
 				<p className="text-sm text-gray-700">
-					© {new Date().getFullYear()} Anime University Society. All
-					rights reserved.
+					© {new Date().getFullYear()} University of Surrey Anime
+					Society. All rights reserved.
 				</p>
 			</div>
 		</footer>
