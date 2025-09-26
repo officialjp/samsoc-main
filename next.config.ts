fe1be0b2c3ba1dba8c18e708558a23c87a4d7c6c
Joao Pedro Porto Zucca da Silva <joaopedroofficial05@gmail.com>
2025-09-26 01:39:08 +0100

    Fuck you supabase

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,10 @@ const nextConfig: NextConfig = {
 				protocol: 'https',
 				hostname: 'cdn.myanimelist.net/**',
 			},
+			{
+				protocol: 'https',
+				hostname: 'heyitsmejp.com/**',
+			},
 		],
 	},
 };
