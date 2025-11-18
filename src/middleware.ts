72cc35ad123981c5f9631e3d147be24874c6242d
Joao Pedro Porto Zucca da Silva <joaopedroofficial05@gmail.com>
2025-11-18 23:14:09 +0000

    Changed env name

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ import { getToken } from 'next-auth/jwt';
 export async function middleware(req: NextRequest) {
 	const token = await getToken({
 		req,
-		secret: process.env.AUTH_SECRET, // Changed from NEXTAUTH_SECRET
+		secret: process.env.NEXTAUTH_SECRET,
 	});
 
 	const isDashboard = req.nextUrl.pathname.startsWith('/dashboard');
